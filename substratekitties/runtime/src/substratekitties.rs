1b7809f87e832cdf1b6db07215e78f32f2a1eb57
Benjamin Wasty <benny.wasty@gmail.com>
2019-04-23 22:43:37 +0200

    before https://shawntabrizi.github.io/substrate-collectables-workshop/#/2/generating-random-data

--- substratekitties/runtime/src/substratekitties.rs
+++ substratekitties/runtime/src/substratekitties.rs
@@ -1,21 +1,39 @@
+use parity_codec::{Decode, Encode};
+use runtime_primitives::traits::{As, Hash};
 use support::{decl_module, decl_storage, dispatch::Result, StorageMap};
 use system::ensure_signed;
+
+#[derive(Encode, Decode, Default, Clone, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub struct Kitty<Hash, Balance> {
+    id: Hash,
+    dna: Hash,
+    price: Balance,
+    gen: u64,
+}
 
 pub trait Trait: balances::Trait {}
 
 #[allow(clippy::redundant_closure)]
 decl_storage! {
     trait Store for Module<T: Trait> as KittyStorage {
-        Value: map T::AccountId => u64;
+        OwnedKitty: map T::AccountId => Kitty<T::Hash, T::Balance>;
     }
 }
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        fn set_value(origin, value: u64) -> Result {
+        fn create_kitty(origin) -> Result {
             let sender = ensure_signed(origin)?;
 
-            <Value<T>>::insert(sender, value);
+            let new_kitty = Kitty {
+                id: <T as system::Trait>::Hashing::hash_of(&0),
+                dna: <T as system::Trait>::Hashing::hash_of(&0),
+                price: <T::Balance as As<u64>>::sa(0),
+                gen: 0,
+            };
+
+            <OwnedKitty<T>>::insert(&sender, new_kitty);
 
             Ok(())
         }
