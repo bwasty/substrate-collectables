57b0a0410bec3bc27fd53a2de06983423171e745
Benjamin Wasty <benny.wasty@gmail.com>
2019-04-30 10:36:06 +0200

    https://shawntabrizi.github.io/substrate-collectables-workshop/#/3/buying-a-kitty

--- substratekitties/runtime/src/substratekitties.rs
+++ substratekitties/runtime/src/substratekitties.rs
@@ -1,9 +1,10 @@
 #![allow(clippy::redundant_closure)]
 
 use parity_codec::{Decode, Encode};
-use runtime_primitives::traits::{As, Hash};
+use runtime_primitives::traits::{As, Hash, Zero};
 use support::{
-    decl_event, decl_module, decl_storage, dispatch::Result, ensure, StorageMap, StorageValue,
+    decl_event, decl_module, decl_storage, dispatch::Result, ensure, traits::Currency, StorageMap,
+    StorageValue,
 };
 use system::ensure_signed;
 
@@ -29,6 +30,8 @@ decl_event!(
     {
         Created(AccountId, Hash),
         PriceSet(AccountId, Hash, Balance),
+        Transferred(AccountId, AccountId, Hash),
+        Bought(AccountId, AccountId, Hash, Balance),
     }
 );
 
@@ -91,6 +94,52 @@ decl_module! {
 
             Ok(())
         }
+
+        fn transfer(origin, to: T::AccountId, kitty_id: T::Hash) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            let owner = Self::owner_of(kitty_id).ok_or("No owner for this kitty")?;
+
+            ensure!(owner == sender, "You do not own this kitty");
+
+            Self::transfer_from(sender, to, kitty_id)?;
+
+            Ok(())
+        }
+
+        fn buy_kitty(origin, kitty_id: T::Hash, max_price: T::Balance) -> Result {
+            let sender = ensure_signed(origin)?;
+
+            ensure!(Kitties::<T>::exists(kitty_id), "kitty doesn't exist");
+
+            let owner = Self::owner_of(kitty_id).ok_or("kitty owner doesn't exist")?;
+            ensure!(owner != sender, "You already are the owner of this kitty");
+
+            let mut kitty = Self::kitty(kitty_id);
+
+            let price = kitty.price;
+            ensure!(!price.is_zero(), "kitty is not for sale");
+
+            ensure!(price <= max_price, "price is greater than max_price");
+
+            <balances::Module<T> as Currency<_>>::transfer(&owner, &sender, price)?;
+
+            Self::transfer_from(owner.clone(), sender.clone(), kitty_id)
+            .expect("`owner` is shown to own the kitty; \
+            `owner` must have greater than 0 kitties, so transfer cannot cause underflow; \
+            `all_kitty_count` shares the same type as `owned_kitty_count` \
+            and minting ensure there won't ever be more than `max()` kitties, \
+            which means transfer cannot cause an overflow; \
+            qed");
+
+            kitty.price = <T::Balance as As<u64>>::sa(0);
+            Kitties::<T>::insert(kitty.id, kitty);
+
+            Self::deposit_event(RawEvent::Bought(sender, owner, kitty_id, price));
+
+            Ok(())
+        }
+
     }
 }
 
@@ -121,6 +170,45 @@ impl<T: Trait> Module<T> {
         OwnedKittiesIndex::<T>::insert(kitty_id, owned_kitty_count);
 
         Self::deposit_event(RawEvent::Created(to, kitty_id));
+
+        Ok(())
+    }
+
+    fn transfer_from(from: T::AccountId, to: T::AccountId, kitty_id: T::Hash) -> Result {
+        let owner = Self::owner_of(kitty_id).ok_or("kitty has no owner")?;
+
+        ensure!(owner == from, "You are not the owner of this kitty");
+
+        let owned_kitty_count_from = Self::owned_kitty_count(&from);
+        let owned_kitty_count_to = Self::owned_kitty_count(&to);
+
+        let new_owned_kitty_count_to = owned_kitty_count_to.checked_add(1).ok_or("int overflow")?;
+        let new_owned_kitty_count_from = owned_kitty_count_from
+            .checked_sub(1)
+            .ok_or("int underflow")?;
+
+        // NOTE: This is the "swap and pop" algorithm we have added for you
+        //       We use our storage items to help simplify the removal of elements from the OwnedKittiesArray
+        //       We switch the last element of OwnedKittiesArray with the element we want to remove
+        let kitty_index = <OwnedKittiesIndex<T>>::get(kitty_id);
+        if kitty_index != new_owned_kitty_count_from {
+            let last_kitty_id =
+                <OwnedKittiesArray<T>>::get((from.clone(), new_owned_kitty_count_from));
+            <OwnedKittiesArray<T>>::insert((from.clone(), kitty_index), last_kitty_id);
+            <OwnedKittiesIndex<T>>::insert(last_kitty_id, kitty_index);
+        }
+        // Now we can remove this item by removing the last element
+
+        KittyOwner::<T>::insert(&kitty_id, &to);
+        OwnedKittiesIndex::<T>::insert(kitty_id, owned_kitty_count_to);
+
+        OwnedKittiesArray::<T>::remove((from.clone(), new_owned_kitty_count_from));
+        OwnedKittiesArray::<T>::insert((to.clone(), owned_kitty_count_to), kitty_id);
+
+        OwnedKittiesCount::<T>::insert(&from, new_owned_kitty_count_from);
+        OwnedKittiesCount::<T>::insert(&to, new_owned_kitty_count_to);
+
+        Self::deposit_event(RawEvent::Transferred(from, to, kitty_id));
 
         Ok(())
     }
